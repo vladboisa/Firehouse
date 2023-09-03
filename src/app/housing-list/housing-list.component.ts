01e7f45f8afeb69b4e824b650388c07162c75c2e
VladBoisa <vladboisa@gmail.com>
2023-09-03 13:21:34 +0200

    feat:add input for receive value from parent comp

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HousingLocation } from './../housing-location';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-housing-list',
@@ -8,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class HousingListComponent implements OnInit {
 
   constructor() { }
+  @Input() locationList:HousingLocation[] = [];
 
   ngOnInit(): void {
   }
-
+  searchHousingLocations(searchText:string) {
+  }
 }
