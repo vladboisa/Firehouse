2933e949f064015a4e0431faa0796fcb7eb3d17a
VladBoisa <vladboisa@gmail.com>
2023-09-13 11:45:07 +0200

    feat: update search location

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -4,15 +4,22 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-housing-list',
   templateUrl: './housing-list.component.html',
-  styleUrls: ['./housing-list.component.css']
+  styleUrls: ['./housing-list.component.css'],
 })
 export class HousingListComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+  @Input() locationList: HousingLocation[] = [];
 
-  constructor() { }
-  @Input() locationList:HousingLocation[] = [];
-
-  ngOnInit(): void {
+  results: HousingLocation[] = [];
+  searchHousingLocations(searchText: string) {
+    if (!searchText) return;
+    this.results = this.locationList.filter((location: HousingLocation) =>
+      location.city.toLowerCase().includes(searchText.toLowerCase())
+    );
   }
-  searchHousingLocations(searchText:string) {
+  @Output() locationSelectedEvent = new EventEmitter<HousingLocation>();
+  selectHousingLocations(location: HousingLocation) {
+    this.locationSelectedEvent.emit(location);
   }
 }
