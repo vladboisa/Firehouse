49d683e8ccdc478c0e8caddb0b3a06743139bf1d
VladBoisa <vladboisa@gmail.com>
2023-09-17 14:02:33 +0200

    feat: add dogs list component & layout

---
+++ src/app/dogs-list.component.ts
@@ -0,0 +1,38 @@
+import { DogsService } from './dogs.service';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DogsListCardComponent } from './dogs-list-card.component';
+
+@Component({
+  selector: 'app-dogs-list',
+  standalone: true,
+  imports: [CommonModule,DogsListCardComponent],
+  template: `
+<section class="hero-section">
+  <h2 class="hero-text">Discover pets</h2>
+  <article class="pet-list">
+    <app-dogs-list-card *ngFor="let dog of dogsService.dogs" [dog]="dog"></app-dogs-list-card>
+  </article>
+</section>
+  `,
+  styles: [`
+
+  .pet-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    padding: 10px;
+  }
+
+  .hero-text {
+    font-size: 25pt;
+    padding: 10px;
+  }
+  `]
+})
+export class DogsListComponent {
+constructor (readonly dogsService:DogsService){
+
+}
+
+}
