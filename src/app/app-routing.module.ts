0f84d75b1ad41ccab0772f0aecbca4af8581b6c4
VladBoisa <vladboisa@gmail.com>
2023-09-19 18:05:46 +0200

    update: router paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { DogsListComponent } from './dogs-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DogViewComponent } from './dog-view.component';
 
 const routes: Routes = [
   {path:'',pathMatch:'full',redirectTo:'list'},
-  {path: 'list', component: DogsListComponent}
+  {path: 'list', component: DogsListComponent},
+  {path: 'details/:index', component: DogViewComponent}
 ];
 
 @NgModule({
