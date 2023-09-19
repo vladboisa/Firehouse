8281824857dafb4a21deb35ee2becfc88e47ce73
VladBoisa <vladboisa@gmail.com>
2023-09-19 18:05:24 +0200

    feat:add styles&link for injecting index route prm

--- src/app/dogs-list-card.component.ts
+++ src/app/dogs-list-card.component.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 
 import { DogsService } from './dogs.service';
 import { Dog } from './dogs.service';
@@ -7,25 +8,54 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-dogs-list-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
-    <ul>
-      <article class="pet-card">
-    <p class="pet-headline">Meet <span class="pet-name">{{dog.name}}</span></p>
-    <p class="pet-description">
-      <span class="pet-name">{{dog.ownerName}}</span> wants you to know this about {{dog.name}}:
-      {{dog.description}}
-    </p>
-    <!-- <p class="pet-learn-more"><a href="/details/{{index}}">Learn More</a></p> -->
-</article>
-    </ul>
+    <article class="pet-card">
+      <p class="pet-headline">
+        Meet <span class="pet-name">{{ dog.name }}</span>
+      </p>
+      <p class="pet-description">
+        <span class="pet-name">{{ dog.ownerName }}</span> wants you to know this
+        about {{ dog.name }}:
+        {{ dog.description }}
+      </p>
+      <p class="pet-learn-more"><a href="details/{{ index }}">Learn More</a></p>
+    </article>
   `,
   styles: [
-  ]
+    `
+      .pet-card {
+        display: flex;
+        flex-direction: column;
+        border-radius: 10px;
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
+          rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+        width: 300px;
+      }
+
+      .pet-img {
+        border-radius: 10px 10px 0 0;
+        width: 300px;
+      }
+
+      .pet-name {
+        font-weight: bolder;
+      }
+
+      .pet-description,
+      .pet-headline,
+      .pet-learn-more {
+        padding: 10px;
+      }
+
+      .pet-headline {
+        font-size: 18pt;
+      }
+    `,
+  ],
 })
 export class DogsListCardComponent {
-@Input() dog!: Dog;
-constructor (readonly dogsService:DogsService){
-
-}
+  @Input() dog!: Dog;
+  @Input() index!: Number;
+  constructor() {}
 }
