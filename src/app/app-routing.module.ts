472c6b080c3a2a92043495c947b7eb78b50078f3
VladBoisa <vladboisa@gmail.com>
2023-09-20 18:08:57 +0200

    feat: add lazy-load component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DogViewComponent } from './dog-view.component';
 const routes: Routes = [
   {path:'',pathMatch:'full',redirectTo:'list'},
   {path: 'list', component: DogsListComponent},
-  {path: 'details/:index', component: DogViewComponent}
+  {path: 'details/:index', loadComponent: () => import('./dog-view.component').then(m => m.DogViewComponent)}
 ];
 
 @NgModule({
