bdcebe110e4402281a9ea423caa0e3cb622c5b0f
VladBoisa <vladboisa@gmail.com>
2023-09-20 17:35:29 +0200

    feat: add pointer to image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,36 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-   <nav>
-      <div>
-        <img class="logo" src="assets/dog-walker-logo.svg">
-        <h1 class="title">Walk My Dog</h1>
-      </div>
-    </nav>
-      <router-outlet></router-outlet>
+    <router-outlet>
+      <nav>
+        <div>
+          <img class="logo" src="assets/dog-walker-logo.svg" [routerLink]="['/list']">
+          <h1 class="title">Walk My Dog</h1>
+        </div>
+      </nav>
+    </router-outlet>
   `,
   styles: [`
-   nav {
-      display: flex;
-      justify-content: space-between;
-      margin: 20px 10px 10px 10px;
-    }
+      nav {
+        display: flex;
+        justify-content: space-between;
+        margin: 20px 10px 10px 10px;
+      }
 
-    nav div.menu-items {
-      display: flex;
-      width: 10%;
-      justify-content: space-between;
-    }
-    nav > div {
-      display: flex;
-    }
-    .title {
-      font-size: 25pt;
-    }
-    .logo {
-      height: 40px;
-    }
-  `]
+      nav div.menu-items {
+        display: flex;
+        width: 10%;
+        justify-content: space-between;
+      }
+      nav > div {
+        display: flex;
+      }
+      .title {
+        font-size: 25pt;
+      }
+      .logo {
+        height: 40px;
+        cursor: pointer;
+      }
+    `],
 })
 export class AppComponent {
   title = 'walk-my-dog';
