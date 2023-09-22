c78ae626e2d1fae0f85ee0a3a5685b4c509af099
VladBoisa <vladboisa@gmail.com>
2023-09-22 21:15:58 +0200

    feat: add router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HousingListComponent } from './housing-list/housing-list.component';
-
+import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
+const routes: Routes = [];
 @NgModule({
   declarations: [
     AppComponent,
     HousingListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
