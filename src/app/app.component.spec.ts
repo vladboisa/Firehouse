a0ead5afdca7b96ed7db3a76b8eea22c71843c4c
VladBoisa <vladboisa@gmail.com>
2023-09-22 21:15:06 +0200

    feat: delete unnecesary line

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'fairhouse'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fairhouse');
   });
 
   it('should render title', () => {
