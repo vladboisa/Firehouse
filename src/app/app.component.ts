96754b905ea16c88fb4613033286890f807beb9a
VladBoisa <vladboisa@gmail.com>
2023-09-24 14:17:00 +0200

    feat: add checker for child elem&update layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,35 +9,43 @@ import { HousingLocation } from './housing-location';
 export class AppComponent {
   housingLocationList: HousingLocation[] = [
     {
-      name: "Acme Fresh Start Housing",
-      city: "Chicago",
-      state: "IL",
-      photo: "../assets/housing-1.jpg",
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '../assets/housing-1.jpg',
       availableUnits: 4,
       wifi: true,
       laundry: true,
     },
     {
-      name: "A113 Transitional Housing",
-      city: "Santa Monica",
-      state: "CA",
-      photo: "../assets/housing-2.jpg",
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: '../assets/housing-2.jpg',
       availableUnits: 0,
       wifi: false,
       laundry: true,
     },
     {
-      name: "Warm Beds Housing Support",
-      city: "Juneau",
-      state: "AK",
-      photo: "../assets/housing-3.jpg",
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: '../assets/housing-3.jpg',
       availableUnits: 1,
       wifi: false,
       laundry: false,
-    }
+    },
   ];
-  selectedLocation : HousingLocation | undefined;
-  updateSelectedLocation (location:HousingLocation) {
+  selectedLocation: HousingLocation | undefined;
+  detailsHousingLocations!: Boolean;
+  searchResults!: HousingLocation[] | undefined;
+  updateSelectedLocation(location: HousingLocation) {
     this.selectedLocation = location;
+  }
+  updatedetailsHousingLocations(detailHouses: Boolean) {
+    this.detailsHousingLocations = detailHouses;
+  }
+  updateSearchResultsLocations(results: HousingLocation[]) {
+    this.searchResults = results;
   }
 }
