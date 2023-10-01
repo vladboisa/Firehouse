05727b87d1408192799baa8e769739bce3f20b84
VladBoisa <vladboisa@gmail.com>
2023-10-01 21:24:09 +0200

    feat: add housing list card comp

--- src/app/housing-list-card/housing-list-card.component.ts
+++ src/app/housing-list-card/housing-list-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HousingLocationsService } from './../housing-locations.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+
 
 @Component({
   selector: 'app-housing-list-card',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./housing-list-card.component.css']
 })
 export class HousingListCardComponent implements OnInit {
-
-  constructor() { }
-
+  detailHouses!:boolean;
+  @Input() searchedLocations!:HousingLocation;
+  selectedLocation! : HousingLocation[];
+  constructor(private locationServices: HousingLocationsService) { }
   ngOnInit(): void {
+    this.locationServices.currentHousingLocation.subscribe( location => this.selectedLocation = location);
+    this.locationServices.isDetailedLocation$.subscribe(detailed => this.detailHouses = detailed)
   }
 
+
+/*   @Output() locationSelectedEvent = new EventEmitter<HousingLocation>();
+  @Output() detailsHousingLocations = new EventEmitter<Boolean>();
+  selectHousingLocations(location: HousingLocation) {
+    this.locationSelectedEvent.emit(location);
+  }
+  viewDetailsHousingLocations(detailHouses: Boolean) {
+    this.detailsHousingLocations.emit(detailHouses);
+  } */
+  selectHousingLocation(location: HousingLocation){
+    this.locationServices.selectHousingLocations(location as any);
+  }
 }
