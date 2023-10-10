f6573038308d291defbd4d6c5f10428a2de69516
VladBoisa <vladboisa@gmail.com>
2023-10-10 20:20:03 +0200

    init: new project Dog

---
+++ projects/dog/src/app/dogs-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dogs-list',
+  templateUrl: './dogs-list.component.html',
+  styleUrls: ['./dogs-list.component.css']
+})
+export class DogsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
