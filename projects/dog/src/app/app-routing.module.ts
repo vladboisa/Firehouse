db24469b2af6a0807de77f100133fa801e9bc5fd
VladBoisa <vladboisa@gmail.com>
2023-10-15 19:13:49 +0200

    feat:combine routes to the main project

--- projects/dog/src/app/app-routing.module.ts
+++ projects/dog/src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
-import { DogsListComponent } from './dogs-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DogViewComponent } from './dog-view.component';
 
 const routes: Routes = [
   {path:'',pathMatch:'full',redirectTo:'list'},
-  {path: 'list', component: DogsListComponent},
-  {path: 'details/:index', loadComponent: () => import('./dog-view.component').then(m => m.DogViewComponent)}
+  {path: 'details/:index', loadComponent:() => import('projects/dog/src/app/dog-view.component').then(m => m.DogViewComponent)}
 ];
 
 @NgModule({
