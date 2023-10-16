9fd00046cfacbc99eda2ae67e5252cef36f8e7db
VladBoisa <vladboisa@gmail.com>
2023-10-16 19:13:36 +0200

    feat: clear project routing

--- projects/dog/src/app/app-routing.module.ts
+++ projects/dog/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
+import { AppDogComponent } from './app.component';
+import { DogsListComponent } from 'projects/dog/src/app/dogs-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'',pathMatch:'full',redirectTo:'list'},
-  {path: 'details/:index', loadComponent:() => import('projects/dog/src/app/dog-view.component').then(m => m.DogViewComponent)}
+
 ];
 
 @NgModule({
