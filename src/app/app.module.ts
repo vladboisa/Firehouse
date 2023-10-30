963d62ad83dc7a0dc6d431a13d56646d8c585110
VladBoisa <vladboisa@gmail.com>
2023-10-30 19:43:41 +0100

    feat:add routing paths&declare-provide proj Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { DogModule } from 'projects/dog/src/app/app.module';
+import { AppDogComponent } from 'projects/dog/src/app/app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,23 +8,26 @@ import { HousingListComponent } from './housing-list/housing-list.component';
 import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
 import { HousingListCardComponent } from './housing-list-card/housing-list-card.component';
 const routes: Routes = [
-  {path: 'list', loadComponent: () => import('projects/dog/src/app/dogs-list.component').then(x => x.DogsListComponent)},
+  {path: '', pathMatch: 'full', redirectTo: 'index'},
+  {path: 'index', component:HousingListComponent},
+  {path: 'dog', component: AppDogComponent},
+  {path: 'list', loadComponent:() => import('projects/dog/src/app/dogs-list.component').then(m=> m.DogsListComponent)},
+  {path: 'details/:index', loadComponent:() => import('projects/dog/src/app/dog-view.component').then(m => m.DogViewComponent)}
 ];
 @NgModule({
   declarations: [
     AppComponent,
     HousingListComponent,
     HousingListCardComponent,
+    AppDogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(routes),
-    RouterModule.forChild(routes),
-    DogModule,
   ],
-  providers: [],
+  providers: [DogModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
