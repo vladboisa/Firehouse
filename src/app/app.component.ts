122917a6b00da892ae91a2d1de3d3ec6e0eacc76
VladBoisa <vladboisa@gmail.com>
2023-10-30 19:44:56 +0100

    feat:add obs$ for singleLocation&refact func logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,11 +18,16 @@ export class AppComponent {
   }
   constructor(private locationServices: HousingLocationsService) {}
   ngOnInit(): void {
-    this.locationServices.currentHousingLocation$.subscribe(
-      (el) => (this.selectedLocation = el as any)
+    this.locationServices.currentHousingLocations$.subscribe(
+      (el) => {
+        if (this.detailsHousingLocations) this.locationServices.takeDetailsHousingLocation(false);
+        this.searchResults = el;}
     );
     this.locationServices.currentDetailedLocation$.subscribe((detailed) => {
       this.detailsHousingLocations = detailed;
     });
+    this.locationServices.singleLocationSelectedSource.subscribe(el => {
+      return this.selectedLocation = el
+    })
   }
 }
