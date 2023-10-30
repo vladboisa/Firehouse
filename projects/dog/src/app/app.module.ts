df476aca65b337aaf284547d78329bd90b20e520
VladBoisa <vladboisa@gmail.com>
2023-10-30 19:39:51 +0100

    refactor: change providers&remove declarations

--- projects/dog/src/app/app.module.ts
+++ projects/dog/src/app/app.module.ts
@@ -1,22 +1,21 @@
+import { AppComponent } from './../../../../src/app/app.component';
 import { RouterModule } from '@angular/router';
 
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class DogModule { }
